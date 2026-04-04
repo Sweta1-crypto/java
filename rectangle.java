893344ffe559a983fdcea71671429ff4241ee3ee
Sweta1-crypto <swetasingh1156@gmail.com>
2026-04-04 23:33:22 +0530

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,12 @@
+package anudip;
+
+public class rectangle {
+public static void main(String[] args)
+{
+	int length = 5;
+	int breadth = 6;
+	int area= length*breadth;
+	System.out.print("Area of rectangle:"+ area);;
+	}
+
+}
