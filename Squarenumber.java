bc3aa06e1c406ad7c8aecbd9187238d6eb8206d6
Sweta1-crypto <swetasingh1156@gmail.com>
2026-04-04 23:33:54 +0530

    Add files via upload

---
+++ Squarenumber.java
@@ -0,0 +1,10 @@
+package anudip;
+
+public class Squarenumber {
+	public static void main(String[] args)
+	{
+		double num = 7.2;
+		double square = num*num;
+	System.out.print("square:" + square);
+}
+}
