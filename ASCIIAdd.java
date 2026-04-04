e9b5346d4cfaf910f71972c0b4e3b3b290ed378d
Sweta1-crypto <swetasingh1156@gmail.com>
2026-04-04 22:42:12 +0530

    Add files via upload

---
+++ ASCIIAdd.java
@@ -0,0 +1,10 @@
+package anudip;
+
+public class ASCIIAdd {
+	public static void main(String[] args)
+	{
+		char ch ='d';
+		char result = (char)(ch+3);
+	System.out.print("Equivalent character :" + result );
+}
+}
