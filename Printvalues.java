cfd057220b34df47f177d244f2997072efa2d945
Sweta1-crypto <swetasingh1156@gmail.com>
2026-04-04 23:32:20 +0530

    Add files via upload

---
+++ Printvalues.java
@@ -0,0 +1,13 @@
+package anudip;
+
+public class Printvalues {
+	public static void main(String[] args)
+	{
+		int a = 10;
+		double b = 100.235;
+		char c ='S';
+		System.out.print("int :" + a);
+		System.out.print("double :" + b);
+		System.out.print("char:" + c);
+				}
+}
