43fa220d7711de2397d1ebb7beb8edcc5d7b44b2
Sweta1-crypto <swetasingh1156@gmail.com>
2026-04-04 22:44:51 +0530

    Add files via upload

---
+++ DoubleToInt.java
@@ -0,0 +1,9 @@
+package anudip;
+
+public class DoubleToInt {public static void main(String[] args)
+{
+	double num = 100.232;
+	int value = (int)num;
+System.out.print("converted value:" + value);
+}
+}
