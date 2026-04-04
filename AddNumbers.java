de4f479eb2184b9b676d4d25d9a6df0e85bab159
Sweta1-crypto <swetasingh1156@gmail.com>
2026-04-04 22:41:21 +0530

    Add files via upload

---
+++ AddNumbers.java
@@ -0,0 +1,11 @@
+package anudip;
+
+public class AddNumbers {
+	public static void main(String[] args)
+	{
+		int a= 5;
+		double b = 7.2;
+		double sum = (a+b);
+	System.out.print("sum:" + sum );
+}
+}
